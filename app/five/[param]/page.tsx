3bfba00816ece51df05275ae893090a0c95c4e52
Thien Do <5953369+thien-do@users.noreply.github.com>
2022-11-27 21:48:28 +0700

    add five

---
+++ app/five/[param]/page.tsx
@@ -0,0 +1,21 @@
+async function fetchData(params: { id: string }) {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${params.id}`,
+    { next: { revalidate: 15 } }
+  );
+  const data = await res.json();
+  return data;
+}
+
+const Five = async (props: any): Promise<JSX.Element> => {
+  const data = await fetchData(props.params);
+
+  return (
+    <div>
+      <div>{new Date().toLocaleTimeString()}</div>
+      <div>{JSON.stringify(data)}</div>
+    </div>
+  );
+};
+
+export default Five;
